511f4fa0af319dd089944018f5d498341e67d1db
Xscientio <xscientio@gmail.com>
2025-02-12 19:20:08 +0400

    Used qsort to sort the list so it could match more accurately & tried to do the keyword & indetifiers toekization but didnt worked well hence its on next time

--- main.c
+++ main.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include "include/tokenizer.h"
+#include <malloc.h>
+#include <string.h>
+#include <stdlib.h>
+
 
 int main()
 {
-char  src_data[] ="print();";
+
+char  src_data[] ="int k =2;";
 char *ptr_src_data =src_data;
 
 
--- src/tokenizor.c
+++ src/tokenizor.c
@@ -3,16 +3,22 @@
 #include <string.h>
 #include <malloc.h>
 #include <stdbool.h>
-char *keywords[]= {"exit","print","printf","clock","int","inttt"} ;
+#include <ctype.h>
+#include <stdlib.h>
+
+char *keywords[]= {"int"} ;
+
 char *puntuators[]={"(",")",";","."};
 char *operators[]={"=","+","-","*","/"};
 char *integers[]={"0","1","2","3","4","5","6","7","8","9"};
-
+char *functions[]={"print","printf"};
 
 int size_keywords =sizeof(keywords)/sizeof(keywords[0]);
+
 int size_puntuators =sizeof(puntuators)/sizeof(puntuators[0]);
 int size_operators =sizeof(operators)/sizeof(operators[0]);
 int size_integers =sizeof(integers)/sizeof(integers[0]);
+int size_functions =sizeof(functions)/sizeof(functions[0]);
 
 typedef struct 
 {
@@ -28,6 +34,12 @@ typedef enum{
     OPERATOR
 
 }TokensType; 
+
+
+int compare_desc(const void *a, const void *b) {
+        return strlen(*(char**)b) - strlen(*(char**)a); // Compare based on length, in reverse order
+ // To sort in descending order
+}
 
 
 
@@ -50,7 +62,7 @@ char*  remove_spaces(char * src_data){
             temp_data[size++] =src_data[i];
 
 
-        }else if(src_data[i]==' '&& !is_string){
+        }else if(src_data[i]=='\n'&& !is_string){
 
         }else{
             temp_data[size++] =src_data[i];
@@ -68,19 +80,26 @@ void core_tokenizer(char *ptr_src_data){
 
 
 
+    qsort(keywords, size_keywords, sizeof(keywords[0]), compare_desc);
+    qsort(functions, size_functions, sizeof(functions[0]), compare_desc);
 
 
-
-
+    // for (int i =0;i<size_functions;i++){
+    //     printf("%s\n",functions[i]);
+    // }
     int size =0, capacity=20;
     char *temp_data = malloc(sizeof(char)*capacity);
     ptr_src_data= remove_spaces(ptr_src_data);
     bool is_string =false;
-    bool is_identifier =false;
+    bool is_alpahabet =false;
     int chosen_one =0;
     bool found_keyword =false;             
 
     printf("%s\n",ptr_src_data);
+
+
+
+
 
 
     for (int i =0; ptr_src_data[i]!='\0';i++){
@@ -95,18 +114,18 @@ void core_tokenizer(char *ptr_src_data){
 
       
                   
-            
-            
-        
+       
 
+
+        if(isalpha(ptr_src_data[i])==0&&ptr_src_data[i]!='_'){
         for (int k=0 ; k<size_puntuators;k++){
-                    if((ptr_src_data[i]==puntuators[k][0])&&!is_string&&!is_identifier){
+                    if((ptr_src_data[i]==puntuators[k][0])&&!is_string){
                             printf("PUNTUATOR %s\n",temp_data);
                             size=0;
                     }
                 }
 
-        if(ptr_src_data[i]=='"'&&ptr_src_data[i-1]!='\\'&&!is_identifier){
+        if(ptr_src_data[i]=='"'&&ptr_src_data[i-1]!='\\'){
                 is_string =!is_string;
                 if(!is_string){
                     printf("STRINGS %s\n",temp_data);
@@ -116,7 +135,7 @@ void core_tokenizer(char *ptr_src_data){
         
         for (int k =0; k<size_integers;k++){
 
-                if(ptr_src_data[i]==integers[k][0]&&!is_string&&!is_identifier){
+                if(ptr_src_data[i]==integers[k][0]&&!is_string){
                         printf("INTEGERS %s\n",temp_data);
                         size =0;
 
@@ -126,7 +145,7 @@ void core_tokenizer(char *ptr_src_data){
         }
 
         for (int k=0;k<size_operators;k++){
-            if(ptr_src_data[i]==operators[k][0]&&!is_string&&!is_identifier){
+            if(ptr_src_data[i]==operators[k][0]&&!is_string){
            
 
                         printf("OPERATOR %s\n",temp_data);
@@ -136,13 +155,13 @@ void core_tokenizer(char *ptr_src_data){
 
 
             }
-        }
-
-
-
+        }}
+        
+                
        
 
     }
 
 
-}
+};
+
