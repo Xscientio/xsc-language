281379de0337839627a0687f4ebb8ce42f77b683
Xscientio <xscientio@gmail.com>
2025-03-08 23:16:38 +0400

    HA

---
+++ src/error/error.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "../include/token_value/token_value.h"
+
+void show_token_specific_error(TokensStored tokens,int token_position,char *message[]){
+
+
+
+}
--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -3,6 +3,7 @@
 #include "../include/debug/debug.h"
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 int sel_token =0;
 TokensStored* tokens_stored;
 int total_tokens;
@@ -18,8 +19,25 @@ void variable_initilize_parser(){
         if (tokens_stored[sel_token].subtype == EQUAL){
             sel_token++;
             if(tokens_stored[sel_token].subtype==USR_INTEGER){
-                for (int i =sel_token; tokens_stored[i].subtype ==USR_INTEGER;i++){
+                for(int x =sel_token; tokens_stored[x].subtype!=COLON && x<total_tokens;x++){
+                
+                if ( tokens_stored[x].type ==OPERATOR){
+                        if(tokens_stored[x+1].type != INTEGER && IDENTIFIER && OPERATOR ){
+                            fprintf(stderr, "\n\033[31mOperand Error\033[0m: Expected an operand after \033[32m'%s'\033[0m either integer or identifier at \033[36mline\033[0m \033[31m%d\033[0m,\033[36mcolumn\033[0m \033[31m%d\033[0m\n\n",tokens_stored[x].value,tokens_stored[x].line,tokens_stored[x].column);
+                            return;
+                        }else{
                         sel_token ++;
+
+                        }
+                }else if(tokens_stored[x].subtype ==USR_INTEGER){
+                        sel_token ++;
+
+                }
+                else{
+
+                }
+
+               
                 }
                 
 
@@ -31,8 +49,8 @@ void variable_initilize_parser(){
             }
         }else if(tokens_stored[sel_token].subtype==COLON){
 
-        }else{
-            printf("Error You missed something");
+        }else{ 
+            printf("Error You missed something\n");
         }
     }
 
@@ -48,7 +66,7 @@ void keyword_distributor(){
 
 
 void core_parser(TokensStored* tokens,int token_count){
-print_tokens(tokens,token_count);
+// print_tokens(tokens,token_count);
 tokens_stored=tokens;
 total_tokens =token_count;
 for (int i =0; i<token_count;i++){
