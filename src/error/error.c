d1acf2104e1ceb08d7b6d0100e8701c6923581b0
Xscientio <xscientio@gmail.com>
2025-03-09 15:15:58 +0400

    lmao

--- src/error/error.c
+++ src/error/error.c
@@ -6,7 +6,7 @@
 void operand_error(int token_position){
     
     bool token_column_foun = false;
-    fprintf(stderr, "\n\033[31mOperand Error\033[0m: Expected an operand after \033[32m'%s'\033[0m at \033[36mline\033[0m \033[31m%d\033[0m,\033[36mcolumn\033[0m \033[31m%d\033[0m either integer or identifier\n\n",
+    fprintf(stderr, "\n\033[31mOperand Error\033[0m: Expected a valid operand after \033[32m'%s'\033[0m at \033[36mline\033[0m \033[31m%d\033[0m,\033[36mcolumn\033[0m \033[31m%d\033[0m\n\n",
     tokens[token_position].value,tokens[token_position].line,tokens[token_position].column);
     fprintf(stderr,"   %d | ",tokens[token_position].line);
     for (int x =0;x< token_count;x++){
@@ -15,7 +15,11 @@ void operand_error(int token_position){
              fprintf(stderr,"\033[31m%s\033[0m ",tokens[x].value);
 
 
-             }else{
+             }else if(x==token_position+1){
+             fprintf(stderr,"\033[35m%s\033[0m ",tokens[x].value);
+
+             }
+             else{
              fprintf(stderr,"%s ",tokens[x].value);          
              }
 
@@ -23,8 +27,44 @@ void operand_error(int token_position){
         }
     }
     fprintf(stderr,"\n     | ");
-    
-    fprintf(stderr,"^");
+    for (int i =0; i<token_position;i++){
+        if(tokens[i].line ==tokens[token_position].line){
+            for (int x =0 ; x< tokens[i].length;x++){
+                fprintf(stderr," ");
+
+            }
+                fprintf(stderr," ");
+
+            
+        }
+    }
+    fprintf(stderr,"  \033[35m^\033[0m");
+    fprintf(stderr,"\n     | ");
+    for (int i =0; i<token_position;i++){
+        if(tokens[i].line ==tokens[token_position].line){
+            for (int x =0 ; x< tokens[i].length;x++){
+                fprintf(stderr," ");
+
+            }
+                fprintf(stderr," ");
+
+            
+        }
+    }
+    fprintf(stderr,"  \033[35m|\033[0m");
+fprintf(stderr,"\n     | ");
+    for (int i =0; i<token_position;i++){
+        if(tokens[i].line ==tokens[token_position].line){
+            for (int x =0 ; x< tokens[i].length;x++){
+                fprintf(stderr," ");
+
+            }
+                fprintf(stderr," ");
+
+            
+        }
+    }
+    fprintf(stderr,"  \033[35m~~ Invalid operand\033[0m");
 
     fprintf(stderr,"\n\n");
 
--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -9,75 +9,13 @@
 
 
 
-
-
-int variable_initilize_parser(int i){
-     i++;
-    if(tokens[i].subtype == USR_IDENTIFIER){
-
-        i++;
-        if (tokens[i].subtype == EQUAL){
-
-            i++;
-            if(tokens[i].subtype==USR_INTEGER){
-
-                for(int x =i; tokens[x].subtype!=COLON && x<token_count;x++){
-                if ( tokens[x].type ==OPERATOR){
-
-
-                        if(tokens[x+1].type != INTEGER && IDENTIFIER && OPERATOR ){
-
-                            operand_error(x);
-                        }else{
-                        i ++;
-
-
-                        }
-                }else if(tokens[x].subtype ==USR_INTEGER){
-                        i ++;
-
-                }
-                else{
-
-                }
-
-               
-                }
-                
-
-            }else if (tokens[i].subtype==USR_STRING){
-                
-
-            }else if (tokens[i].subtype==USR_IDENTIFIER){
-
-            }
-        }else if(tokens[i].subtype==COLON){
-
-        }else{ 
-            printf("Error You missed something\n");
-        }
-    }
-    return i;
-
-}
-
-
-int keyword_distributor(int i){
-    if (tokens[i].subtype == INT || STR){
-
-        variable_initilize_parser(i);
-    }
-    return i;
-}
-
-
-
 void core_parser(){
-
+// print_tokens(tokens,token_count);
 for (int i =0; i<token_count;i++){
+
     if(tokens[i].type==KEYWORD){
-        // printf("%s\n",tokens[i].value);
-      i=  keyword_distributor(i);
+
+    }else if(tokens[i].type==IDENTIFIER){
 
     }
       
